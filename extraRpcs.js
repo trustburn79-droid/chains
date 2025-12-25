67da665ba6f6e628e0e13f26bf3d8c57565eaf1c
trustburn79-droid <trustburn79@gmail.com>
2025-12-26 00:03:33 +0900

    Create extraRpcs.js

---
+++ extraRpcs.js
@@ -0,0 +1,27 @@
+// TBURN Mainnet (Chain ID: 6000)
+6000: {
+  rpcs: [
+    "https://tburn.io/rpc"
+  ],
+  chainName: "TBURN Mainnet",
+  nativeCurrency: {
+    name: "TBURN",
+    symbol: "TBURN",
+    decimals: 18
+  },
+  blockExplorerUrls: ["https://tburn.io/scan"]
+},
+
+// TBURN Testnet (Chain ID: 5900)
+5900: {
+  rpcs: [
+    "https://tburn.io/testnet-rpc"
+  ],
+  chainName: "TBURN Testnet",
+  nativeCurrency: {
+    name: "TBURN",
+    symbol: "TBURN",
+    decimals: 18
+  },
+  blockExplorerUrls: ["https://tburn.io/testnet-scan"]
+}
